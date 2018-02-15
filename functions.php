ca709e8f57a841afc406ad63de97c2221859f738
Sven Seeberg <sven@geeq.de>
2018-02-15 20:56:08 +0100

    define config.ini structure

--- functions.php
+++ functions.php
@@ -1,3 +1,12 @@
 <?php
 
+function encrypt_password($password, $salt=false) {
+    if(false == $salt) {
+        $salt = substr(sha1(rand()), 0, 16);
+    }
+    $hashed_password = "{SHA512-CRYPT}" . crypt($password, "$6$$salt");
+    return $hashed_password;
+}
+
+
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-$config = parse_ini_file ( 'config.ini' );
+$config = parse_ini_file ( 'config.ini', $process_sections = true);
 var_dump($config);
+$mysqli = new mysqli($config['db']['host'], $config['db']['user'], $config['db']['password'], $config['db']['database']);
+start_session();
+var_dump(session_id());
 ?>
