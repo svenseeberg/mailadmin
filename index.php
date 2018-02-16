8d8624faf3b3251c2ff5c6473af3505aa33895e6
Sven Seeberg <sseebergelverfeldt@suse.com>
2018-02-16 19:09:23 +0100

    Structure HTTP Post parsing

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ require_once('functions.php');
 $cfg = init();
 $cfg = verify_logged_in($cfg,$_POST['user'],$_POST['password']);
 if(is_string($cfg['username'])) {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        parse_post();
+    }
     draw('menu', $cfg);
 } else {
     draw('login', $cfg);
