7fbde9db2a8a6f35dbe719d1d757a80004605c11
Sven Seeberg <sseebergelverfeldt@suse.com>
2018-02-16 18:48:25 +0100

    Restructure login functions

--- functions.php
+++ functions.php
@@ -28,12 +28,12 @@ function login($cfg, $user, $password) {
     $query = "SELECT id, password FROM accounts WHERE username=? AND domain=? LIMIT 1";
     $stmt = $cfg['mysqli']->prepare($query);
     $stmt->bind_param('ss', $address[0], $address[1]);
-    $stmt->bind_result($id, $db_password);
+    $stmt->bind_result($user_id, $db_password);
     $stmt->execute();
     $stmt->fetch();
     $stmt->close();
     if(verify_password($db_password, $password))
-        return $id;
+        return $user_id;
     else
         return false;
 }
@@ -51,22 +51,9 @@ function set_user_domain($cfg, $user_id) {
     return $cfg;
 }
 
-function verify_logged_in($cfg, $user=false, $password=false) {
-    if(is_string($user) and is_string($password) and substr_count($user, '@') == 1){
-        $user_id = login($cfg, $user, $password);
-        $sid = session_id();
-        $time = time();
-        if($user_id) {
-            $query = "INSERT INTO logins (id, session_id, timeout) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE session_id=?, timeout=?";
-            $stmt = $cfg['mysqli']->prepare($query);
-            $timeout = $time + 600;
-            $stmt->bind_param("issss", $user_id, $sid, $timeout, $sid, $timeout);
-            $stmt->execute();
-            $stmt->close();
-        } else {
-            echo "Try again.";
-        }
-    }
+function get_current_user_id($cfg) {
+    $sid = session_id();
+    $time = time();
     $query = "SELECT id FROM logins WHERE session_id=? AND timeout>=?";
     $stmt = $cfg['mysqli']->prepare($query);
     $stmt->bind_param('si',$sid,$time);
@@ -74,6 +61,28 @@ function verify_logged_in($cfg, $user=false, $password=false) {
     $stmt->bind_result($user_id);
     $stmt->fetch();
     $stmt->close();
+    return $user_id;
+}
+
+function save_session($cfg, $user_id) {
+    $sid = session_id();
+    $timeout = time() + 600;
+    $query = "INSERT INTO logins (id, session_id, timeout) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE session_id=?, timeout=?";
+    $stmt = $cfg['mysqli']->prepare($query);
+    $stmt->bind_param("issss", $user_id, $sid, $timeout, $sid, $timeout);
+    $stmt->execute();
+    $stmt->close();
+}
+
+function verify_logged_in($cfg, $user=false, $password=false) {
+    if(is_string($user) and is_string($password) and substr_count($user, '@') == 1){
+        $user_id = login($cfg, $user, $password);
+        if($user_id)
+            save_session($cfg, $user_id);
+        else
+            echo "Try again.";
+    }
+    $user_id = get_current_user_id($cfg);
     if($user_id) {
         $cfg = set_user_domain($cfg, $user_id);
         return $cfg;
