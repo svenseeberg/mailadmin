d7c85ad0788d3136c89c22317874fef0e3443937
Sven Seeberg <sven@geeq.de>
2018-02-18 17:08:30 +0100

    working edit

--- functions.php
+++ functions.php
@@ -47,6 +47,28 @@ function get_db_password_id($cfg, $user_id) {
     return $db_password;
 }
 
+function get_account_domain($cfg, $user_id) {
+    $query = "SELECT domain FROM accounts WHERE id=? LIMIT 1";
+    $stmt = $cfg['mysqli']->prepare($query);
+    $stmt->bind_param('s', $user_id);
+    $stmt->bind_result($domain);
+    $stmt->execute();
+    $stmt->fetch();
+    $stmt->close();
+    return $domain;
+}
+
+function get_alias_domain($cfg, $alias_id) {
+    $query = "SELECT source_domain FROM aliases WHERE id=? LIMIT 1";
+    $stmt = $cfg['mysqli']->prepare($query);
+    $stmt->bind_param('s', $alias_id);
+    $stmt->bind_result($domain);
+    $stmt->execute();
+    $stmt->fetch();
+    $stmt->close();
+    return $domain;
+}
+
 function login($cfg, $address, $password) {
     list($user_id, $db_password) = get_db_password_address($cfg, $address);
     if(verify_password($db_password, $password))
@@ -64,6 +86,7 @@ function set_user_domain($cfg, $user_id) {
     $stmt->fetch();
     $cfg['username'] = $username;
     $cfg['userdomain'] = $domain;
+    $cfg['user_id'] = $user_id;
     $stmt->close();
     return $cfg;
 }
@@ -112,20 +135,8 @@ function draw($page, $cfg) {
     include("templates/".$page.".php");
 }
 
-function parse_action($cfg) {
-    if(array_key_exists('new_user_address' ,$_POST)) {
-        new_user($cfg, $_POST['new_user_address'], $_POST['new_usesr_password'], $cfg['defaults']['quota'], $_POST['new_user_enabled'], $_POST['new_user_sendonly']);
-    } elseif(array_key_exists('new_alias_source' ,$_POST)) {
-        new_alias($cfg, $_POST['new_alias_source'], $_POST['new_alias_destination'], $_POST['new_alias_enabled']);
-    } elseif(array_key_exists('edit_user' ,$_POST)) {
-        update_user($cfg, $_POST['edit_user'], $_POST['edit_user_password'], $_POST['edit_user_quota'], $_POST['edit_user_enabled'], $_POST['edit_user_sendonly']);
-    } elseif(array_key_exists('edit_alias' ,$_POST)) {
-        update_alias($cfg, $_POST['edit_alias'], $_POST['edit_alias_destination'], $_POST['edit_alias_enabled']);
-    }
-}
-
 function parse_page($cfg) {
-    if(strlen($_GET['path']) > 0) {
+    if(array_key_exists('path' ,$_GET) and strlen($_GET['path']) > 0) {
         $parts = explode('/', $_GET['path']);
         $cfg['page'] = $parts[0];
         $cfg['edit'] = $parts[1];
@@ -187,23 +198,114 @@ function list_aliases($cfg, $id=false) {
     return $user_list;
 }
 
-function update_password($old_password, $new_password, $new_password2) {
-
+function parse_action($cfg) {
+    if(array_key_exists('new_user_address' ,$_POST) and array_key_exists('new_user_password' ,$_POST)) {
+        new_user(           $cfg, $_POST['new_user_address'],
+                            $_POST['new_user_password'],
+                            $cfg['defaults']['quota'],
+                            (array_key_exists('new_user_enabled', $_POST) ? 1 : 0),
+                            (array_key_exists('new_user_sendonly', $_POST) ? 1 : 0)
+                );
+    } elseif(array_key_exists('new_alias_source' ,$_POST) and array_key_exists('new_alias_destination' ,$_POST)) {
+        new_alias(          $cfg, $_POST['new_alias_source'],
+                            $_POST['new_alias_destination'],
+                            (array_key_exists('new_alias_enabled', $_POST) ? 1 : 0)
+                 );
+    } elseif(array_key_exists('edit_user' ,$_POST)) {
+        update_user(        $cfg, $_POST['edit_user'],
+                            $_POST['edit_user_password'],
+                            $_POST['edit_user_quota'],
+                            (array_key_exists('edit_user_enabled', $_POST) ? 1 : 0),
+                            (array_key_exists('edit_user_sendonly', $_POST) ? 1 : 0)
+                   );
+    } elseif(array_key_exists('edit_alias' ,$_POST)) {
+        update_alias(       $cfg,
+                            $_POST['edit_alias'],
+                            $_POST['edit_alias_destination'],
+                            (array_key_exists('edit_alias_enabled', $_POST) ? 1 : 0)
+                    );
+    } elseif(array_key_exists('old_pw' ,$_POST)) {
+        update_password(    $cfg,
+                            $_POST['old_pw'],
+                            $_POST['new_pw1'],
+                            $_POST['new_pw2']
+                       );
+    }
 }
 
-function new_user($cfg, $address, $password, $quota, $enabled, $sendonly) {
+function update_password($cfg, $old_password, $new_password, $new_password2) {
+    $db_password = get_db_password_id($cfg, $cfg['user_id']);
+    if($new_password == $new_password2 and verify_password($db_password, $old_password)) {
+        $query = "UPDATE accounts SET password=? WHERE id=?";
+        $stmt = $cfg['mysqli']->prepare($query);
+        $encrypted_new_password = encrypt_password($new_password);
+        $stmt->bind_param("si", $encrypted_new_password, $cfg['user_id']);
+        $stmt->execute();
+        $stmt->close();
+        return true;
+    }
+    return false;
+}
 
+function new_user($cfg, $address, $password, $quota, $enabled=0, $sendonly=0) {
+    $address = explode('@', $address);
+    if(sizeof($address)==2 and in_array($address[1], $cfg['admin_domains'])) {
+        $query = "INSERT INTO accounts (username, domain, password, quota, enabled, sendonly) VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt = $cfg['mysqli']->prepare($query);
+        $encrypted_password = encrypt_password($password);
+        $stmt->bind_param("sssiii", $address[0], $address[1], $encrypted_password, $quota, $enabled, $sendonly);
+        $stmt->execute();
+        $stmt->close();
+        return true;
+    }
+    return false;
 }
 
 function update_user($cfg, $user_id, $password, $quota, $enabled, $sendonly) {
- 
+    $domain = get_account_domain($cfg, $user_id);
+    if(in_array($domain, $cfg['admin_domains'])) {
+        if(sizeof($password) > 8) {
+            $encrypted_password = encrypt_password($password);
+            $query = "UPDATE accounts SET password=?, quota=?, enabled=?, sendonly=? WHERE id=?";
+            $stmt = $cfg['mysqli']->prepare($query);
+            $stmt->bind_param("siiii", $encrypted_password, $quota, $enabled, $sendonly, $user_id);
+        } else {
+            $query = "UPDATE accounts SET quota=?, enabled=?, sendonly=? WHERE id=?";
+            $stmt = $cfg['mysqli']->prepare($query);
+            $stmt->bind_param("iiii", $quota, $enabled, $sendonly, $user_id);
+        }
+        $stmt->execute();
+        $stmt->close();
+        return true;
+    }
+    return false;
 }
 
 function new_alias($cfg, $source, $destination, $enabled) {
- 
+    $source = explode('@', $source);
+    $destination = explode('@', $destination);
+    if(sizeof($source)==2 and sizeof($destination)==2 and in_array($source[1], $cfg['admin_domains'])) {
+        $query = "INSERT INTO aliases (source_username, source_domain, destination_username, destination_domain, enabled) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $cfg['mysqli']->prepare($query);
+        $stmt->bind_param("ssssi", $source[0], $source[1], $destination[0], $destination[1], $enabled);
+        $stmt->execute();
+        $stmt->close();
+        return true;
+    }
+    return false;
 }
 
-function update_alias($cfg, $source, $destination, $enabled) {
- 
+function update_alias($cfg, $alias_id, $destination, $enabled) {
+    $domain = get_alias_domain($cfg, $alias_id);
+    $destination = explode('@', $destination);
+    if(sizeof($destination)==2 and in_array($domain, $cfg['admin_domains'])) {
+        $query = "UPDATE aliases SET destination_username=?, destination_domain=?, enabled=? WHERE id=?";
+        $stmt = $cfg['mysqli']->prepare($query);
+        $stmt->bind_param("ssii", $destination[0], $destination[1], $enabled, $alias_id);
+        $stmt->execute();
+        $stmt->close();
+        return true;
+    }
+    return false;
 }
 ?>
--- templates/edit.php
+++ templates/edit.php
@@ -8,6 +8,11 @@
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+            $("#edit_user_password").val("");
+            });
+</script>
   </head>
 <body>
 <div class="container">
@@ -17,14 +22,15 @@
    $user = list_users($cfg, $cfg['item'])[0];
    ?>
    <h3>Edit User</h3>
-   <form method='post' action='/'>
+   <form method='post' action='' autocomplete="new-password" >
     <input name="edit_user" type="hidden" value="<?php echo $user['id']; ?>"/>
     <table class="table">
      <tr><th>User</th><th><?php echo $user['address']; ?></th><th></th></tr>
-     <tr><td>Password</td><td><input name="edit_user_password" type="password" class="form-control" placeholder="Set password" /></td><td></td></tr>
+     <tr><td>Password</td><td><input id="edit_user_password" name="edit_user_password" type="text" class="form-control" placeholder="Set password" autocomplete="new-password" value=""/></td><td></td></tr>
      <tr><td>Quota</td><td><input name="edit_user_quota" type="text" class="form-control" placeholder="Current password" value="<?php echo $user['quota']; ?>" /></td><td>MB</td></tr>
      <tr><td>Enabled</td><td><input name="edit_user_enabled" type="checkbox" class="form-check-input" value="1" <?php if($user['enabled']) echo "checked"; ?>/></td><td></td></tr>
      <tr><td>Send Only</td><td><input name="edit_user_sendonly" type="checkbox" class="form-check-input" value="1" <?php if($user['sendonly']) echo "checked"; ?>/></td><td></td></tr>
+     <tr><td><input type="submit" class="btn btn-info" value="Save"></td><td></td><td></td><td></td></tr>
     </table>
    </form>
    <?php
@@ -32,12 +38,13 @@
    $alias = list_aliases($cfg, $cfg['item'])[0];
    ?>
    <h3>Edit Alias</h3>
-   <form method='post' action='/'>
-    <input name="edit_alias" type="hidden" value="<?php echo $user['id']; ?>"/>
+   <form method='post' action='' autocomplete="off">
+    <input name="edit_alias" type="hidden" value="<?php echo $alias['id']; ?>"/>
     <table class="table">
      <tr><th>Source</th><th><?php echo $alias['source']; ?></th><th></th></tr>
      <tr><td>Destination</td><td><input name="edit_alias_destination" type="text" class="form-control" placeholder="Set password" value="<?php echo $alias['destination']; ?>"/></td><td></td></tr>
-     <tr><td>Enabled</td><td><input name="edit_user_enabled" type="checkbox" class="form-check-input" id="new_mail_sendonly"value="1" <?php if($user['enabled']) echo "checked"; ?>/></td><td></td></tr>
+     <tr><td>Enabled</td><td><input name="edit_alias_enabled" type="checkbox" class="form-check-input" id="new_mail_sendonly"value="1" <?php if($alias['enabled']) echo "checked"; ?>/></td><td></td></tr>
+     <tr><td><input type="submit" class="btn btn-info" value="Save"></td><td></td><td></td></tr>
     </table>
    </form>
    <?php
