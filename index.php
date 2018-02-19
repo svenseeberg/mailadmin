c576ad8daee4669afa1595ef1151e381b63353a3
Sven Seeberg <sven@geeq.de>
2018-02-19 20:34:21 +0100

    Fix logout

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
     require_once('functions.php');
     $cfg = init();
 
+    if($_GET['path'] == 'logout') {
+        logout($cfg);
+    }
+
     $cfg = verify_logged_in($cfg,
         (array_key_exists('user' ,$_POST) ? $_POST['user'] : false),
         (array_key_exists('password' ,$_POST) ? $_POST['password'] : false));
-    if($_GET['path'] == 'logout') {
-        logout($cfg);
-        draw('login', $cfg);
-    } elseif(is_string($cfg['username'])) {
+    if(is_string($cfg['username'])) {
         $cfg = admin_domains($cfg);
         parse_action($cfg);
         $cfg = parse_page($cfg);
